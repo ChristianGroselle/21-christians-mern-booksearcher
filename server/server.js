e81eb477b386731efd9dcf68b0075f6873120fff
Christian Groselle <christian.groselle@gmail.com>
2023-02-22 09:10:24 -0500

    refactored back-end for graphql

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +11,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+const server = new ApolloServer({ typeDefs, resolvers });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const httpServer = require("http").createServer(app);
+
+server.applyMiddleware({ app });
+
+db.once("open", () => {
+  httpServer.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(
+      `GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 });
