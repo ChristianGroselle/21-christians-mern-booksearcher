506b985478f5fb1e247bbe3e43e5810ffab251aa
Christian Groselle <christian.groselle@gmail.com>
2023-02-22 18:31:51 -0500

    adapting front end

--- server/server.js
+++ server/server.js
@@ -15,17 +15,24 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-const httpServer = require("http").createServer(app);
-
-server.applyMiddleware({ app });
-
-db.once("open", () => {
-  httpServer.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(
-      `GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`
-    );
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
